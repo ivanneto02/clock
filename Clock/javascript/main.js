444dd766509015dd54598eaadb050ec4f1cf2a1e
Cody Kurpanek <codykurpanek@gmail.com>
2020-12-09 20:13:48 -0800

    Cody Changes

--- Clock/javascript/main.js
+++ Clock/javascript/main.js
@@ -6,16 +6,6 @@ class clock {
         this.iterate();
     }
 
-    formatTime() {
-        this.currtime = new Date();
-
-        this.hours = this.currtime.getHours();
-        this.minutes = this.currtime.getMinutes();
-        this.seconds = this.currtime.getSeconds();
-
-        return `${this.hours}:${this.minutes}:${this.seconds}`;
-    }
-
     formatDate() {
         this.currdate = new Date();
 
@@ -24,16 +14,43 @@ class clock {
         this.day = this.currdate.getDay();
 
         return `${this.year}-${this.month}-${this.day}`;
+        //return this.currdate.format("hh:mm:ss tt");
+    }
+
+    formatTime() {
+        this.currtime = new Date();
+
+        this.hours = this.currtime.getHours();
+        this.minutes = this.currtime.getMinutes();
+        this.seconds = this.currtime.getSeconds();
+
+        return `${this.pad(this.hours)}:${this.pad(this.minutes)}:${this.pad(this.seconds)}: this`;
+    }
+
+    //added padding to the time so it always shows twice
+    pad(input) {
+        let value = input.toString();
+        console.log("here");
+        console.log(value);
+        console.log(value.length);
+
+        if(value.length < 2 && value.length !=0) {
+            return "0" + value;
+        }
+        return value;
+    }
+
+    det(hour) {
+        if(hour>10) {
+            return "AM"
+        }
     }
 
     iterate() {
-        document.getElementById("time").innerHTML = "Current date: " + this.formatDate() + "<br>" + "Current time: " + this.formatTime();
-        
+        document.getElementById("time").innerHTML = "Current date: " + this.formatDate() + "<br>" + "Current time: " + this.formatTime();       
         setTimeout(() => {
             this.iterate();
         }, 1000);
-        
-
     }
 }
 
